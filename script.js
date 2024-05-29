d165b3f95c75a158eb4c99e28535501f782019ec
Helge-R <109223097+Helge-R@users.noreply.github.com>
2024-05-29 15:53:48 +0200

    Create operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber  = '';
+let operator     = '';
+let secondNumber = '';
+
 
 /* Basic math operator functions */
 function add(num1, num2) {
@@ -11,4 +15,21 @@ function multiply(num1, num2) {
 }
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+/* Operate */
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "add":
+      return add(num1, num2);
+      break;
+    case "subtract":
+      return subtract(num1, num2);
+      break;
+    case "multiply":
+      return multiply(num1, num2);
+      break;
+    case "divide":
+      return divide(num1, num2);
+  }
 }
