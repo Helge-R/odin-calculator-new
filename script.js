27ba6369f79d5f6b776f28230344739ab65622d1
Helge-R <109223097+Helge-R@users.noreply.github.com>
2024-05-31 11:36:56 +0200

    Finish basic functionality and fix common bugs

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 let previousValue  = '';
 let operator       = '';
 let currentValue   = '';
+let hasResult = false;
+let previousScreen = '';
+let currentScreen = '';
 
 // Setup when HTML document has been completely parsed
 document.addEventListener("DOMContentLoaded", function() {
 
   let numbers = document.querySelectorAll(".number");
+  let operators = document.querySelectorAll(".operator");
+  let equal = document.querySelector(".equal");
+  let clear = document.querySelector("#clear-button")
 
-  let currentScreen = document.querySelector(".current");
+  currentScreen = document.querySelector(".current");
+  previousScreen = document.querySelector(".previous");
 
   numbers.forEach((number) => number.addEventListener("click", function(e){
     handleNumber(e.target.textContent);
     currentScreen.textContent = currentValue;
   }));
+
+  operators.forEach((op) => op.addEventListener("click", function(e){
+    if (operator !== "") {
+      operate(Number(previousValue), Number(currentValue), operator);
+      currentScreen.textContent = currentValue;
+    } 
+
+    handleOperator(e.target.textContent);
+    previousScreen.textContent = previousValue + " " + operator;
+  
+  }));
+
+  equal.addEventListener("click", function(e){
+    if (previousValue !== '' && currentValue !== '' && operator !== '') {
+      let currentOld = currentValue;
+      operate(Number(previousValue), Number(currentValue), operator);
+      currentScreen.textContent = currentValue;
+      previousScreen.textContent = previousValue + " " + operator + " " + currentOld + " " + "=";
+      hasResult = true;
+    }
+    
+  });
+
+  clear.addEventListener("click", function(e){
+    clearScreen();
+  })
 
 });
 
@@ -32,23 +65,49 @@ function divide(num1, num2) {
 }
 
 /* Operate */
-function operate(num1, num2, operator) {
-  switch (operator) {
-    case "add":
-      return add(num1, num2);
+function operate(num1, num2, op) {
+  let result = 0;
+  switch (op) {
+    case "+":
+      result = add(num1, num2);
       break;
-    case "subtract":
-      return subtract(num1, num2);
+    case "-":
+      result = subtract(num1, num2);
       break;
-    case "multiply":
-      return multiply(num1, num2);
+    case "x":
+      result = multiply(num1, num2);
       break;
-    case "divide":
-      return divide(num1, num2);
+    case "/":
+      result = divide(num1, num2);
+      break;
   }
+  currentValue = roundNumber(result).toString();
 }
 
 
 function handleNumber(num) {
+  if (hasResult) {
+    hasResult = false;
+    clearScreen();
+  }
   currentValue += num;
+}
+
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = '';
+
+}
+
+function clearScreen() {
+    previousValue = '';
+    previousScreen.textContent = '';
+    currentValue = '';
+    currentScreen.textContent = 0;
+    operator = '';
+}
+
+function roundNumber(num) {
+  return Math.round(num * 1000) / 1000;
 }
